653d4152c0e67462f20422f7a738f23d5f8fc2d4
MihalinaGotovchits <Mix-Mihalina@ya.ru>
2024-11-30 18:12:14 +0300

    Вернуть ручную установку в StatServiceApplication

--- stat-service/stat_server/src/main/java/ru/practicum/StatServiceApplication.java
+++ stat-service/stat_server/src/main/java/ru/practicum/StatServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StatServiceApplication {
     public static void main(String[] args) {
+        System.setProperty("server.port", "9090");
         SpringApplication.run(StatServiceApplication.class, args);
     }
 }
