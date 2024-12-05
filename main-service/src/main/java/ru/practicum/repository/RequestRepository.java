e3f1b3cf0c91ae9fa4a203c8a06b4df99dbd6709
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2024-12-05 19:39:40 +0300

    Добавить RequestRepository

---
+++ main-service/src/main/java/ru/practicum/repository/RequestRepository.java
@@ -0,0 +1,15 @@
+package ru.practicum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.model.Request;
+import ru.practicum.status.request.RequestStatus;
+
+import java.util.List;
+
+public interface RequestRepository extends JpaRepository<Request, Long> {
+    /**метод находит все запросы на основе списока идентификаторов событий (eventIds).
+     * и status — указывает статус запроса, по которому также будет выполняться фильтрация.
+     Метод возвращает список объектов Request — все запросы, которые соответствуют переданным идентификаторам
+     событий и указанному статусу.*/
+    List<Request> findAllByEventIdInAndStatus(List<Long> eventIds, RequestStatus status);
+}
