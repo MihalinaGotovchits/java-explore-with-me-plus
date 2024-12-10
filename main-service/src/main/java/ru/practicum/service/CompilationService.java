ff419483890439f71f1d1c53affd197e6b291336
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2024-12-10 13:10:12 +0300

    Добавила public логику

--- main-service/src/main/java/ru/practicum/service/CompilationService.java
+++ main-service/src/main/java/ru/practicum/service/CompilationService.java
@@ -4,10 +4,16 @@ import ru.practicum.dto.compilation.CompilationDto;
 import ru.practicum.dto.compilation.NewCompilationDto;
 import ru.practicum.dto.compilation.UpdateCompilationRequest;
 
+import java.util.List;
+
 public interface CompilationService {
     CompilationDto addCompilation(NewCompilationDto compilationDto);
 
     CompilationDto updateCompilation(Long compId, UpdateCompilationRequest updateComp);
 
     void deleteCompilation(Long compId);
+
+    List<CompilationDto> getCompilations(Boolean pinned, Integer from, Integer size);
+
+    CompilationDto findByIdCompilation(Long compId);
 }
