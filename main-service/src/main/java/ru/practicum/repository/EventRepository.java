5fdb9f35ac2d9398978602e9979d0b1df8dd02fe
MihalinaGotovchits <Mix-Mihalina@yandex.ru>
2024-12-12 15:11:53 +0300

    Добавила параметр

--- main-service/src/main/java/ru/practicum/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/repository/EventRepository.java
@@ -1,16 +1,19 @@
 package ru.practicum.repository;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ru.practicum.model.Category;
 import ru.practicum.model.Event;
 
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
 import java.util.List;
 
-public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor {
+public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
 
     List<Event> findByCategory(Category category);
 
     List<Event> findAllByIdIn(List<Long> eventIds);
-
 }
